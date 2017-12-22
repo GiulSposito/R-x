9a834ff57244ad39a24e8884b52a2f5e6da9e336
Giuliano Sposito <gsposito@ciandt.com>
2017-12-22 14:01:15 -0200

    Renaming ignoring and new tests

--- GetHFData/hello_ibov.R
+++ GetHFData/hello_ibov.R
@@ -1,31 +1,18 @@
+# reproducing this code: https://msperlin.github.io/2017-01-18-GetHFData/
+
+
 # the FTP download is downloaded at the root
-# change the working dir 
+# change the working dir instead
 setwd("./GetHFData/")
 
-# script to test the basics of GetFHData
-install.packages("GetHFData")
-
-# load libs
+# setup
 library(GetHFData)
 library(tidyverse)
 
-# dates to import
-dates <- c("2017-12-18","2017-12-19","2017-12-20","2017-12-21")
-ftp <- list()
 
-# download the FTPs files (one for each date)
-ftp <- lapply(dates, function(x) ghfd_get_available_tickers_from_ftp(x, "BMF")[1:5,1:2] )
-names(ftp) <- dates
+n.assets <- 6
+my.date <- as.Date('2017-12-19')
+type.market <- 'equity'
 
-# bind the dataframes
-FTP <- do.call(rbind, ftp)
-FTP <- FTP %>% 
-  mutate(data = row.names(.),
-         data = gsub("\\.\\d", "", data),
-         data = as.Date(data),
-         tickers = as.character(tickers))
-
-# plot the number of observations
-ggplot(FTP, aes(x = data, y = n.obs, group = tickers)) +
-  geom_line(aes(colour = tickers)) +
-  theme_classic()
+df.tickers <- ghfd_get_available_tickers_from_ftp(my.date = my.date, 
+                                                  type.market = type.market)
