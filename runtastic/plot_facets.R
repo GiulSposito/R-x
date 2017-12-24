409ab2f3bd0951d1f21c6932b28ec769beccd506
Giuliano Sposito <gsposito@ciandt.com>
2017-12-24 13:48:40 -0200

    saving the plot

--- runtastic/plot_facets.R
+++ runtastic/plot_facets.R
@@ -9,11 +9,13 @@ library(tidyverse)
 data <- readRDS("./data/gpx_processed.rds")
 
 # Create plot
-ggplot() +
-  geom_path(aes(lon, lat, group = id), data, size = 0.35, lineend = "round") +
-  facet_wrap(~id, scales = "free", labeller = function(...){return("")}) +
-  theme_void() +
-  theme(panel.spacing = unit(0, "lines"))
+p <- data %>%
+  ggplot() +
+    geom_path(aes(lon, lat, group = id), size = 0.35, lineend = "round") +
+    facet_wrap(~id, scales = "free", labeller = function(...){return("")}) +
+    theme_void() +
+    theme(panel.spacing = unit(0, "lines"))
+
 
 # Save plot
-ggsave("./plots/facets001.png", p, width = 20, height = 20, units = "cm")
+ggsave("./facets001.png", p, width = 20, height = 20, units = "cm")
