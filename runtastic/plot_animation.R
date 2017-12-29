731ac2df9687d77821d029d0d8e1e0a30109271c
Giuliano Sposito <gsposito@ciandt.com>
2017-12-29 11:00:23 -0200

    clean up and better visualization

--- runtastic/plot_animation.R
+++ runtastic/plot_animation.R
@@ -1,44 +1,29 @@
 # set working dir
 # setwd("./runtastic")
 
-library(tidyverse)
+library(lubridate)
 library(ggplot2)
 library(gganimate)
-library(lubridate)
+library(tidyverse)
 
 data <- readRDS("./data/gpx_processed.rds")
-
-data %>%
-  filter( year(time)==2017 ) %>%
-  mutate( data.time = floor_date(time, unit="3 minutes") ) %>%
-  group_by(id, data.time) %>%
-  summarise( lat = min(lat), 
-             lon = min(lon), 
-             ele = min(ele),
-             dist_to_prev = min(dist_to_prev), 
-             cumdist = min(cumdist),
-             cumtime = min(cumtime) ) %>% 
-  ungroup() %>%
-  mutate( id = as.factor(id) ) %>% 
-  arrange( id, data.time ) %>% ggplot() +
-  geom_path(aes(lon, lat, group=id, color=id, frame=data.time, cumulative=T),
-            alpha = 0.3, size = 0.7, lineend = "round") +
-  coord_fixed() +
-  theme_void() +
-  theme( legend.position = "none" ) -> p
-
-gganimate(p, interval=0.005, ani.width=800, ani.height=400)
-
 
 #### tentando arredondar sem sumir com os pontos
 data %>%
   filter( year(time)==2017 ) %>%
   mutate( data.time = floor_date(time, unit="5 minutes") ) %>%
   mutate( id = as.factor(id) ) %>% 
-  arrange( id, data.time ) %>%
+  arrange( data.time ) %>% 
+  head(60000) %>%
   ggplot() +
-  geom_path(aes(lon, lat, group=id, color=id, frame=data.time, cumulative=T),
-            alpha = 0.3, size = 0.7, lineend = "round") +
+  geom_path(aes(lon, lat, group=id, frame=data.time, cumulative=T),
+            alpha = 0.2, size = 0.8, lineend = "round") +
+  geom_path(aes(lon, lat, frame=data.time, cumulative=F),
+            size=1.2, lineend = "round", color="red") +
   coord_fixed() +
   theme_void() +
   theme( legend.position = "none" ) -> p
+
+p
+
+gganimate(p, interval=0.003, ani.width=800, ani.height=400)
