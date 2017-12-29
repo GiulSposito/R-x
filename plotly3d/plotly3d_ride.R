7c46b86e60a76bd6a90cc554db1bec38da9dbf19
Giuliano Sposito <gsposito@ciandt.com>
2017-12-29 18:51:42 -0200

    plotando os 10 mais longos caminhos em 3D com plotly

--- plotly3d/plotly3d_ride.R
+++ plotly3d/plotly3d_ride.R
@@ -15,13 +15,20 @@ data %>%
              min.ele = min(ele, na.rm = T), 
              mean.ele = mean(ele, na.rm=T), 
              sd.ele   = sd(ele, na.rm = ), 
-             total.dist = max(cumdist, na.rm=T)) %>%
-  arrange( max.ele, min.ele ) %>%
-  View()
+             total.dist = max(cumdist, na.rm=T),
+             total.dur  = max( cumtime )) %>%
+  arrange( desc(total.dist) ) -> ids
+
+View(ids)
+# id %in% c(34,35,26,32,8,12,14),
+
+
+
 
 ## p
 data %>%
-  filter(id %in% c(34,35,26,32,8,12,14), ele >= 600, ele <= 1000) %>%
+  filter( ele >= 550, ele <= 1200,
+          (id %in% ids$id[1:10])) %>%
   mutate( id = as.factor(id),
           lon = -lon,
           ele.smth = despike(ele, reference = "median",n=0.2,k=101) ) %>%
@@ -30,5 +37,19 @@ data %>%
   layout(scene = list(xaxis = list(title = 'lat'),
                       yaxis = list(title = 'lon'),
                       zaxis = list(title = 'ele'),
-                      aspectratio = list(x=1, y=2, z=0.5)))
-?layout
+                      aspectratio = list(x=1, y=2, z=0.2))) -> p
+
+
+p
+
+# remenber to follow the account setup: https://plot.ly/r/getting-started/
+link <- api_create(p, sharing = "public")
+
+# chart_link = plotly_POST(p, filename="runtastic/elevation")
+
+# 22.68624-22.65500   -> 3000 km
+# 0.03124 -> 300
+
+
+
+
