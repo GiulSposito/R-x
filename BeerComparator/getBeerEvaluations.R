2a1948f92ef72dcf99fb168029d520359a7e3ece
Giuliano Sposito <gsposito@ciandt.com>
2018-01-30 14:55:32 -0200

    Code to scrap one page

--- BeerComparator/getBeerEvaluations.R
+++ BeerComparator/getBeerEvaluations.R
@@ -2,45 +2,66 @@
 library(rvest)
 library(stringr)
 library(tidyverse)
+library(lubridate)
 
 base.url <- "https://cervanossa.wordpress.com/"
 pages <- 1:50
 
-html_doc <- read_html(base.url)
+scrapBeerPage <- function(base.url) {
 
-html_doc %>% 
-  html_nodes("div .post") %>%
-  html_nodes("h2 a:first-child") %>%
-  html_text() %>%
-  as.tibble() %>%
-  rename(nome=value) -> beers.name
+  html_doc <- read_html(base.url)
+  
+  html_doc %>% 
+    html_nodes("div .post") %>%
+    html_nodes("h2 a:first-child") %>%
+    html_text() %>%
+    as.tibble() %>%
+    rename(nome=value) -> beers.name
+  
+  html_doc %>% 
+    html_nodes("div .post") %>%
+    html_nodes("h2 a:first-child") %>%
+    html_attr("href") %>%
+    as.tibble() %>%
+    rename(link.avaliacao=value) -> beers.eval_link
+  
+  html_doc %>% 
+    html_nodes(".main p:first-child img") %>%
+    html_attr("src") %>%
+    str_replace("\\?.*","") %>%
+    as.tibble() %>% 
+    rename(image=value) -> beers.image
+  
+  html_doc %>%
+    html_nodes(".main p:first-child") %>%
+    html_text() %>% str_split("\n") %>%
+    map(function(texts){
+      str_replace(texts, ".+: ", "") %>%
+        str_replace(.,"\\.+$","") 
+    }) %>% unlist() %>% as.vector() %>%
+    matrix(ncol=9, byrow = T) %>%
+    as.tibble() %>%
+    setNames(c("pais","tipo","alcool",
+               "cor","sabor","malte",
+               "avaliacao","preco","volume")) -> beers.eval
+  
+  html_doc %>%
+    html_nodes(".main p:last-of-type a") %>%
+    html_attr("href") %>%
+    as.tibble() %>%
+    rename(url=value) -> beers.url
+  
+  html_doc %>%
+    html_nodes(".signature p:last-of-type") %>%
+    html_text() %>%
+    dmy_hm() %>%
+    as.tibble() %>%
+    rename(data.avalicao=value) -> beers.eval_date
+  
+  bind_cols( beers.name, beers.eval, beers.eval_date,
+             beers.eval_link, beers.image, beers.url) %>% return()
 
-html_doc %>% 
-  html_nodes(".main p:first-child img") %>%
-  html_attr("src") %>%
-  str_replace("\\?.*","") %>%
-  as.tibble() %>% 
-  rename(image=value) -> beers.image
+}
 
-html_doc %>%
-  html_nodes(".main p:first-child") %>%
-  html_text() %>% str_split("\n") %>%
-  map(function(texts){
-    str_replace(texts, ".+: ", "") %>%
-      str_replace(.,"\\.+$","") 
-  }) %>% unlist() %>% as.vector() %>%
-  matrix(ncol=9, byrow = T) %>%
-  as.tibble() %>%
-  setNames(c("pais","tipo","alcool",
-             "cor","sabor","malte",
-             "avaliacao","preco","volume")) -> beers.eval
 
-html_doc %>%
-  html_nodes(".main p:last-of-type a") %>%
-  html_attr("href") %>%
-  as.tibble() %>%
-  rename(url=value) -> beers.url
-
-beers <- bind_cols(beers.name, beers.eval, beers.image, beers.url)
-
-View(beers)
+View(scrapBeerPage(base.url))
