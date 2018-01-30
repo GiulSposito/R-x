1122f47266d8caa13ac02f6ecc74b09988d7427b
Giuliano Sposito <gsposito@ciandt.com>
2018-01-30 14:43:07 -0200

    capturando nome, imagem, url e avalicao montando um tibble.

--- BeerComparator/getBeerEvaluations.R
+++ BeerComparator/getBeerEvaluations.R
@@ -8,25 +8,39 @@ pages <- 1:50
 
 html_doc <- read_html(base.url)
 
-html_doc %>%
+html_doc %>% 
+  html_nodes("div .post") %>%
+  html_nodes("h2 a:first-child") %>%
+  html_text() %>%
+  as.tibble() %>%
+  rename(nome=value) -> beers.name
+
+html_doc %>% 
+  html_nodes(".main p:first-child img") %>%
+  html_attr("src") %>%
+  str_replace("\\?.*","") %>%
+  as.tibble() %>% 
+  rename(image=value) -> beers.image
 
 html_doc %>%
   html_nodes(".main p:first-child") %>%
   html_text() %>% str_split("\n") %>%
   map(function(texts){
-    str_replace(texts, ".+: ", "")
+    str_replace(texts, ".+: ", "") %>%
+      str_replace(.,"\\.+$","") 
   }) %>% unlist() %>% as.vector() %>%
-  matrix(ncol=9, byrow = T) -> atributos
+  matrix(ncol=9, byrow = T) %>%
+  as.tibble() %>%
+  setNames(c("pais","tipo","alcool",
+             "cor","sabor","malte",
+             "avaliacao","preco","volume")) -> beers.eval
 
+html_doc %>%
+  html_nodes(".main p:last-of-type a") %>%
+  html_attr("href") %>%
+  as.tibble() %>%
+  rename(url=value) -> beers.url
 
+beers <- bind_cols(beers.name, beers.eval, beers.image, beers.url)
 
-
-str()
-
-matrix( unlist(beers.eval) )
-
-str(beers.eval)
-
-dimnames = list("pais","tipo","alcool",
-                "cor","sabor","malte",
-                "avaliacao","preco","volume")
+View(beers)
