4fe2f98be0c1712b00058a53478eed6fda999c1e
Giuliano Sposito <gsposito@ciandt.com>
2018-01-30 15:47:21 -0200

    quebrando o nome para cervejaria e cerveja

--- BeerComparator/getBeerEvaluations.R
+++ BeerComparator/getBeerEvaluations.R
@@ -5,10 +5,12 @@ library(tidyverse)
 library(lubridate)
 
 base.url <- "https://cervanossa.wordpress.com/"
-pages <- 1:50
+pages <- 1:8
 
 scrapBeerPage <- function(base.url) {
 
+  print(paste0("Scrapping: ", base.url))
+  
   html_doc <- read_html(base.url)
   
   html_doc %>% 
@@ -16,7 +18,11 @@ scrapBeerPage <- function(base.url) {
     html_nodes("h2 a:first-child") %>%
     html_text() %>%
     as.tibble() %>%
-    rename(nome=value) -> beers.name
+    rename(nome.completo=value) %>%
+    mutate( 
+      cervejaria = str_split(nome.completo," - ", simplify = T)[,1],
+      cerveja = str_split(nome.completo," - ", simplify = T)[,2] 
+    ) -> beers.name
   
   html_doc %>% 
     html_nodes("div .post") %>%
@@ -33,23 +39,24 @@ scrapBeerPage <- function(base.url) {
     rename(image=value) -> beers.image
   
   html_doc %>%
-    html_nodes(".main p:first-child") %>%
+    html_nodes(".main p:first-child") %>% 
     html_text() %>% str_split("\n") %>%
     map(function(texts){
       str_replace(texts, ".+: ", "") %>%
         str_replace(.,"\\.+$","") 
-    }) %>% unlist() %>% as.vector() %>%
+    }) %>% 
+    unlist() %>% as.vector() %>%
     matrix(ncol=9, byrow = T) %>%
     as.tibble() %>%
     setNames(c("pais","tipo","alcool",
                "cor","sabor","malte",
                "avaliacao","preco","volume")) -> beers.eval
   
-  html_doc %>%
-    html_nodes(".main p:last-of-type a") %>%
-    html_attr("href") %>%
-    as.tibble() %>%
-    rename(url=value) -> beers.url
+  # html_doc %>%
+  #   html_nodes(".main p:last-of-type a") %>%
+  #   html_attr("href") %>%
+  #   as.tibble() %>%
+  #   rename(url=value) -> beers.url
   
   html_doc %>%
     html_nodes(".signature p:last-of-type") %>%
@@ -59,9 +66,15 @@ scrapBeerPage <- function(base.url) {
     rename(data.avalicao=value) -> beers.eval_date
   
   bind_cols( beers.name, beers.eval, beers.eval_date,
-             beers.eval_link, beers.image, beers.url) %>% return()
+             beers.eval_link, beers.image) %>% return()
 
 }
 
+pages %>%
+  paste0(base.url, "page/", .) %>%
+  map_df(scrapBeerPage) -> beers
 
-View(scrapBeerPage(base.url))
+
+View(beers)
+
+base.url <- "https://cervanossa.wordpress.com/page/9"
