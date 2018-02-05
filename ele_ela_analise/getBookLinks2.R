555cd16edf713e8c7aaaa4cc8c5aefc3b29f68c7
Giuliano Sposito <gsposito@ciandt.com>
2018-02-05 17:54:53 -0200

    tratando titulos vazios

--- ele_ela_analise/getBookLinks2.R
+++ ele_ela_analise/getBookLinks2.R
@@ -1,16 +1,13 @@
 # scrap web pages to get clear links
-
 library(rvest)
 library(tidyverse)
 library(stringr)
 
 n <- "2112"
-url.books.index <- "http://www.dominiopublico.gov.br/pesquisa/ResultadoPesquisaObraForm.do?first=50&skip=0&ds_titulo=&co_autor=&no_autor=&co_categoria=2&pagina=2&select_action=Submit&co_midia=2&co_obra=&co_idioma=1&colunaOrdenar=null&ordem=null"
-
+url.books.index <- "http://www.dominiopublico.gov.br/pesquisa/ResultadoPesquisaObraForm.do?first=500&skip=0&ds_titulo=&co_autor=&no_autor=&co_categoria=2&pagina=2&select_action=Submit&co_midia=2&co_obra=&co_idioma=1&colunaOrdenar=null&ordem=null"
 # url.book.detail <- "http://www.dominiopublico.gov.br/pesquisa/DetalheObraForm.do?select_action=&co_obra=81912"
 # url.book.download <- "http://www.dominiopublico.gov.br/pesquisa/DetalheObraDownload.do?select_action=&co_midia=2&co_obra=81912"
 url.book.download <- "http://www.dominiopublico.gov.br/pesquisa/DetalheObraDownload.do?select_action=&co_midia=2&co_obra="
-
 
 html.doc <- read_html(url.books.index)
 
@@ -19,7 +16,8 @@ html.doc %>%
   html_table(fill=T, trim=T, header=F) -> books.table
 
 books.table[8:nrow(books.table), 3:8] %>%
-  setNames(c("title","author","source","format","size","access")) -> books.attribs
+  setNames(c("title","author","source","format","size","access")) %>%
+  filter( !is.na(title) | title!="" | title!=" ") -> books.attribs
 
 html.doc %>%
   html_nodes("#res tbody tr td a") %>%
@@ -31,6 +29,7 @@ html.doc %>%
 
 books.attribs %>% 
   inner_join(tibble(title=titles, link=links)) %>%
+  distinct() %>%
   mutate(book.id = str_match(link,".+co_obra=(.+)")[,2]) %>%
   mutate(download = paste0(url.book.download,book.id)) -> books
 
