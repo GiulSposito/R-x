22141beed2c56278f56dff0472470dcd617276ab
Giuliano Sposito <gsposito@ciandt.com>
2018-04-20 14:47:28 -0300

    movendo scripts de test para a pasta infra Testando acesso ao Google Cloud Storage

---
+++ PaulOctopus/R/infra/hello_cloudstorage.R
@@ -0,0 +1,14 @@
+library(googleCloudStorageR)
+
+gcs_get_global_bucket()
+gcs_list_objects("ciandt_projectoctopus_2018_gsposito")
+
+gcs_list_objects()
+
+
+gcs_get_bucket()
+
+gcs_auth()
+gar_auth()
+library(googleAuthR)
+gar_auth()
