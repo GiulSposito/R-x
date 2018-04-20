63587d44c49de254af675fd8f205cbfc2b99d0dd
Giuliano Sposito <gsposito@ciandt.com>
2018-04-20 15:30:42 -0300

    Testes de acesso ao Cloud Storage

--- PaulOctopus/R/infra/hello_cloudstorage.R
+++ PaulOctopus/R/infra/hello_cloudstorage.R
@@ -1,14 +1,46 @@
+# escopo de acesso
+options(googleAuthR.scopes.selected = "https://www.googleapis.com/auth/cloud-platform")
 library(googleCloudStorageR)
+library(tidyverse)
 
-gcs_get_global_bucket()
-gcs_list_objects("ciandt_projectoctopus_2018_gsposito")
-
-gcs_list_objects()
-
-
-gcs_get_bucket()
-
+# forca autenticação via prompt :(
 gcs_auth()
-gar_auth()
-library(googleAuthR)
-gar_auth()
+
+# lista buckets 
+gcs_list_buckets("project-paul-the-octopus")
+
+# obtem o bucket 
+bucket <- gcs_get_bucket("ciandt_projectoctopus_2018_gsposito")
+bucket
+
+# trabalhando com o "bucket" obtido
+objects <- gcs_list_objects()
+objects
+
+# fazendo um uploads
+
+# text
+gcs_upload(file = "./PaulOctopus/README.md",
+           name = "README.md") # content type -> text/x-markdown
+
+
+# csv
+fname <- "./PaulOctopus/export/mtcars.csv"
+write.csv(mtcars, fname)
+gcs_upload(file = fname,
+           name = "mtcars.csv",
+           type = "text/csv")
+
+# list to json
+gcs_upload(list(a=1,b=3,c=list(d=2,e=5)),
+           name = "list.json") # content type -> application/json
+
+# Visualizando
+gcs_list_objects() %>%
+  View()
+
+# removing objects
+gcs_delete_object("mtcars.csv")
+gcs_delete_object("list.json")
+gcs_delete_object("README.md")
+
