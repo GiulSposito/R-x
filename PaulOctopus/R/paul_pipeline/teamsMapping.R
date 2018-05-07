4e6a26ddf7ac883ae73de9bb0a5efedd9b5152c3
Giuliano Sposito <gsposito@ciandt.com>
2018-05-07 13:08:48 -0300

    melhoria de codigo

--- PaulOctopus/R/paul_pipeline/teamsMapping.R
+++ PaulOctopus/R/paul_pipeline/teamsMapping.R
@@ -1,18 +1,27 @@
-readTable("matches",.BQ_PROJ, "paul_the_octopus_dataset") %>%
-  mutate( away = case_when(
-    away == " Switzerland" ~ "Switzerland",
-    TRUE ~ away
-  )) -> matches
+source("./PaulOctopus/R/infra/bigQuery.R")
 
-.tables$teams %>%
-  mutate( team.name = case_when(
-    team.cod == "IR" ~ "IR Iran",
-    team.cod == "KR" ~ "Korea Republic",
-    TRUE ~ team.name
-)) -> teams
-
-matches %>%
-  left_join(teams, by=c("home"="team.name")) %>%
-  rename(home.team.cod = team.cod) %>%
-  left_join(teams, by=c("away"="team.name")) %>%
-  rename(away.team.cod = team.cod) -> matches
+# importa os jogos da copa do dataset original do paul
+# faz o match de team.cod
+importWorldCupMatches <- function(){
+  
+  readTable("matches",.BQ_PROJ, "paul_the_octopus_dataset") %>%
+    mutate( away = case_when(
+      away == " Switzerland" ~ "Switzerland",
+      TRUE ~ away
+    )) -> matches
+  
+  readTable("teams") %>%
+    mutate( team.name = case_when(
+      team.cod == "IR" ~ "IR Iran",
+      team.cod == "KR" ~ "Korea Republic",
+      TRUE ~ team.name
+    )) -> .teams
+  
+  matches %>%
+    left_join(.teams, by=c("home"="team.name")) %>%
+    rename(home.team.cod = team.cod) %>%
+    left_join(.teams, by=c("away"="team.name")) %>%
+    rename(away.team.cod = team.cod) %>%
+    return()
+}
+  
