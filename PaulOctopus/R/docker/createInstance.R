b261c98aa27053c9ea2b35378edd97b8370b9bb7
Giuliano Sposito <gsposito@ciandt.com>
2018-05-07 08:49:20 -0300

    Tentativas de configurar adequadamente o ambiente via Docker

--- PaulOctopus/R/docker/createInstance.R
+++ PaulOctopus/R/docker/createInstance.R
@@ -1,7 +1,30 @@
 library(googleComputeEngineR)
 
-vm <- gce_vm_container(
-  name = "giul-rbase",
-  predefined_type = "n1-highmem-2",
-  file = "./PaulOctopus/docker/r-base-plus/Dockerfile"
-)
+job <- gce_vm_container(#file = "./PaulOctopus/docker/r-base.yaml",
+                       cloud_init = readChar(file("./PaulOctopus/docker/r-base-plus.yaml"),nchars=10000),
+                       name = "giul-my-rbase",
+                       predefined_type = "n1-highmem-2")
+
+
+vm <- gce_vm("giul-my-rbase")
+
+library(future)
+library(future.apply)
+library(tidyverse)
+
+
+vm <- gce_ssh_setup(vm,
+  key.pub = file.path("C:/Users/gsposito/.ssh", "google_compute_engine.pub"),
+  key.private = file.path("C:/Users/gsposito/.ssh", "google_compute_engine"))
+
+plan(cluster, workers=as.cluster(vm))
+
+future_lapply(1, function(x) installed.packages(), future.scheduling = F) %>%
+  .[[1]] %>%
+  as.tibble() %>%
+  arrange(Package) %>%
+  View()
+
+
+gce_vm_stop(vm)
+gce_vm_delete(vm)
--- PaulOctopus/R/infra/create_rstudio.R
+++ PaulOctopus/R/infra/create_rstudio.R
@@ -4,8 +4,10 @@ vm <- gce_vm_template(
                 name = "giul-rstudio2",
                 username = "giul", password = 'biggiul',
                 predefined_type = "n1-highmem-2",
-                file="C:/Users/gsposito/Documents/R/win-library/3.5/googleComputeEngineR/dockerfiles/rstudio-gcloud/")
+                file="C:/Users/gsposito/Documents/R/win-library/3.5/googleComputeEngineR/dockerfiles/rstudiogcloud/")
 
+
+?gce_vm_template()
 
 
 gce_vm_container(
@@ -14,9 +16,17 @@ gce_vm_container(
   cloud_init = readChar(file("C:/Users/gsposito/Documents/R/win-library/3.5/googleComputeEngineR/dockerfiles/rstudio-gcloud/Dockerfile2"),nchars=10000)
 )
 
-vm <- gce_vm("giul-rstudio")
+vm <- gce_vm("giul-rstudio2")
 gce_vm_stop(vm)
 gce_vm_delete(vm)
 
+?gce_vm_template
 
-install.packages(format)
+
+vm <- gce_vm_template(
+  template="dynamic",
+  name = "giul-rstudio",
+  predefined_type = "n1-highmem-2",
+  dynamic_image="gcr.io/project-paul-the-octopus/github-giulsposito-r-x")
+  
+
