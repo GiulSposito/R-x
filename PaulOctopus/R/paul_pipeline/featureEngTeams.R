d0c008c36e630452e5d5a820059b122c63286460
Giuliano Sposito <gsposito@ciandt.com>
2018-05-09 16:12:25 -0300

    saldo de goals e variacoes de rank

--- PaulOctopus/R/paul_pipeline/featureEngTeams.R
+++ PaulOctopus/R/paul_pipeline/featureEngTeams.R
@@ -59,6 +59,15 @@ teams.stats %>%
     goals.agst.L10   = as.integer(rollsum(goals.agst.L1, k=10, na.pad=T, fill=0, align = "right")),
     goals.agst.L20   = as.integer(rollsum(goals.agst.L1, k=20, na.pad=T, fill=0, align = "right"))
   ) %>%
+  mutate( 
+    # net score
+    goals.net.L1 = goals.pro.L1 - goals.agst.L1,
+    goals.net.L2 = goals.pro.L2 - goals.agst.L2,
+    goals.net.L3 = goals.pro.L3 - goals.agst.L3,
+    goals.net.L5 = goals.pro.L5 - goals.agst.L5,
+    goals.net.L10 = goals.pro.L10 - goals.agst.L10,
+    goals.net.L20 = goals.pro.L20 - goals.agst.L20
+  ) %>%
   mutate(
     win.L1     = as.integer(    lag(win,    k=2,  default=0)),
     wins.L2    = as.integer(rollsum(win.L1, k=2,  na.pad=T, fill=0, align = "right")),
@@ -78,6 +87,21 @@ teams.stats %>%
     defeats.L5   = as.integer(rollsum(defeat.L1, k=5,  na.pad=T, fill=0, align = "right")),
     defeats.L10  = as.integer(rollsum(defeat.L1, k=10, na.pad=T, fill=0, align = "right")),
     defeats.L20  = as.integer(rollsum(defeat.L1, k=20, na.pad=T, fill=0, align = "right"))
+  ) %>%
+  mutate(
+    # rank and rating
+    delta.rank.L1    = as.integer(    lag(delta.rank,    k=2,  default=0)),
+    delta.rank.L2    = as.integer(rollsum(delta.rank.L1, k=2,  na.pad=T, fill=0, align = "right")),
+    delta.rank.L3    = as.integer(rollsum(delta.rank.L1, k=3,  na.pad=T, fill=0, align = "right")),
+    delta.rank.L5    = as.integer(rollsum(delta.rank.L1, k=5,  na.pad=T, fill=0, align = "right")),
+    delta.rank.L10   = as.integer(rollsum(delta.rank.L1, k=10, na.pad=T, fill=0, align = "right")),
+    delta.rank.L20   = as.integer(rollsum(delta.rank.L1, k=20, na.pad=T, fill=0, align = "right")),
+    delta.ranting.L1    = as.integer(    lag(delta.rating,    k=2,  default=0)),
+    delta.ranting.L2    = as.integer(rollsum(delta.ranting.L1, k=2,  na.pad=T, fill=0, align = "right")),
+    delta.ranting.L3    = as.integer(rollsum(delta.ranting.L1, k=3,  na.pad=T, fill=0, align = "right")),
+    delta.ranting.L5    = as.integer(rollsum(delta.ranting.L1, k=5,  na.pad=T, fill=0, align = "right")),
+    delta.ranting.L10   = as.integer(rollsum(delta.ranting.L1, k=10, na.pad=T, fill=0, align = "right")),
+    delta.ranting.L20   = as.integer(rollsum(delta.ranting.L1, k=20, na.pad=T, fill=0, align = "right"))
   ) %>%
   filter(team %in% c("BR","GR")) %>%
   View()
