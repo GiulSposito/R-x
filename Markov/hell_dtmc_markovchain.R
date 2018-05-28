836360e52f2e96f83bdf1bb3bcde5390269248ff
Giuliano Sposito <gsposito@ciandt.com>
2018-05-28 14:41:38 -0300

    testando o pacote para fazer MarkovChains

---
+++ Markov/hell_dtmc_markovchain.R
@@ -0,0 +1,44 @@
+library(markovchain)
+
+# transition matrix
+# i -> j
+# row sum must be 1
+tmA <- matrix(c(0,0.5,0.5,
+                0.5,0,0.5,
+                0.5,0.5,0), nrow=3, byrow=T)
+
+# create the DTMC
+dtmcA <- new("markovchain", 
+             transitionMatrix=tmA,
+             states=c("a","b","c"),
+             name="MarkovChainA")
+
+# plot it
+plot(dtmcA)
+
+# probability from state 1 to state 2
+dtmcA[1,2]
+
+# transition probability by state names  (b->c)
+transitionProbability(dtmcA, "b","c")
+
+# simulation: from "initial state", after 4 steps
+inicialState <- c(0,1,0)
+steps <- 4
+finalState <- inicialState * dtmcA^steps
+
+# result
+finalState
+
+# steadyState (equilibrium)
+steadyStates(dtmcA)
+
+# resumo
+summary(dtmcA)
+
+# estimating the transition probabilities
+data(rain)
+mysequence <- rain$rain
+createSequenceMatrix(mysequence)
+myFit<-markovchainFit(data=mysequence,confidencelevel = .9,method = "mle")
+myFit
