fb95aa0c3956073ac7f5ad72ad773c45f9f716e5
Giuliano Sposito <gsposito@ciandt.com>
2018-10-05 10:52:42 -0300

    Predictions para week 5 Comparativo Pred x Real

--- FantasyFootball/ffanalytics.R
+++ FantasyFootball/ffanalytics.R
@@ -6,13 +6,15 @@ my_scrap <- scrape_data(src = c("CBS", "Yahoo", "ESPN", "NFL", "FFToday",
                                 "FantasyFootballNerd", "FleaFlicker"), 
                         pos = c("QB", "RB", "WR", "TE", "DST"),
                         season = 2018, 
-                        week = 4)
+                        week = 5)
 
 
-list("week4" = my_scrap) %>% 
-  saveRDS("weekly_scrap.rds")
+my_scrap %>% 
+  saveRDS("./FantasyFootball/weekly_scrap.rds")
 
-my_projections <- projections_table(my_scrap, scoring_rules = dudes.score.settings)
+projections_table(my_scrap, scoring_rules = dudes.score.settings) %>% 
+  saveRDS("./FantasyFootball/my_projects.rds")
+
 my_stats <- ffanalytics::aggregate_stats(my_scrap)
 
 my_projections$avg_type %>% unique()
@@ -81,3 +83,71 @@ my_projections %>%
   coord_flip()
 
 my_projections %>% pull(avg_type) %>% unique()  
+
+
+
+
+my_projections <- w4_projetions %>% 
+  filter(avg_type=="weighted") %>% 
+  add_player_info() %>% 
+  mutate( full_name = paste0(first_name, " ", last_name),
+          tier = as.factor(tier) ) %>%
+  group_by( id ) %>% 
+  mutate( r_pos = max(ceiling, points) ) %>% 
+  ungroup() %>% 
+  filter( !(team %in% c("FA","FA*")) )
+
+
+map(results, function(rslt){
+  rslt %>% 
+    select(id, weekpts) %>% 
+    return()
+}) %>%
+  bind_rows() %>%
+  inner_join(my_projections) -> my_projections
+
+
+plotProjections <- function(.proj, .pos, .title, .subtitle) {
+  proj_data <- .proj %>% 
+    filter(position==.pos) %>% 
+    arrange(pos_rank) %>% 
+    head(30)
+  
+  nu_y <- max(proj_data$ceiling, na.rm = T) / 21
+  
+  proj_data %>% 
+    ggplot(aes(group=tier)) +
+    geom_pointrange(aes(x=reorder(pos_rank,desc(pos_rank)),
+                        y=points, 
+                        ymin=floor, 
+                        ymax=ceiling, 
+                        color=tier)) +
+    geom_point(aes(x=reorder(pos_rank,desc(pos_rank)),
+                   y=weekpts,
+                   color=tier), shape=8, size=1) +
+    geom_text(aes(x=reorder(pos_rank,desc(pos_rank)), 
+                  y=r_pos, 
+                  color=tier, 
+                  label=last_name), nudge_y = nu_y, nudge_x = 0.1, size=2.5) +
+    theme_classic() + xlab("Rank") + ylab("Points") +
+    theme( panel.grid.major.x=element_line(colour="#EEEEEE"),
+           legend.position="none" ) +
+    scale_x_discrete(breaks = seq(1,32,4) ) +
+    ggtitle(.title,.subtitle) +
+    coord_flip()
+}  
+
+plotProjections(my_projections, "QB", "QB", "week 4")
+ggsave("./FantasyFootball/qb.png",device = "png", width = 8, height = 8, units="in")
+
+plotProjections(my_projections, "WR", "WR", "week 4")
+ggsave("./FantasyFootball/wr.png",device = "png", width = 8, height = 8, units="in")
+
+plotProjections(my_projections, "RB", "RB", "week 4")
+ggsave("./FantasyFootball/rb.png",device = "png", width = 8, height = 8, units="in")
+
+plotProjections(my_projections, "TE", "TE", "week 4")
+ggsave("./FantasyFootball/te.png",device = "png", width = 8, height = 8, units="in")
+
+plotProjections(my_projections, "DST", "DEF", "week 4")
+ggsave("./FantasyFootball/def.png",device = "png", width = 8, height = 8, units="in")
