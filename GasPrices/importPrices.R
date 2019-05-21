5c9f214031f9fe105e9b2907dc1af9f4153b8cc9
Giuliano Sposito <gsposito@ciandt.com>
2019-05-21 14:17:48 -0300

    separando mutate_at as.factor() importando varios arquivos de varios anos

--- GasPrices/importPrices.R
+++ GasPrices/importPrices.R
@@ -1,12 +1,34 @@
 library(tidyverse)
+library(lubridate)
 
-read_lines("./GasPrices/data/2018-2_CA.csv", skip = 1) %>%
-  as_tibble() %>% 
-  separate(value, into=c("regiao","estado","municipio", "posto",
-                         "instalacao", "produto", "data","valor_compra",
-                         "valor_venda","unidade","distribuidor"), sep="  ") %>% 
-  mutate_at(vars(regiao, estado, municipio, posto, produto, unidade, distribuidor), as.factor) %>% 
-  mutate_at(vars(valor_compra, valor_venda), str_replace, ",", ".") %>% 
-  mutate_at(vars(valor_compra, valor_venda), as.numeric) %>% 
-  mutate_at(vars(instalacao), as.integer) %>% 
+dir("./GasPrices/data",pattern = ".*zip", full.names = T) %>% 
+  sort() %>% 
+  map(function(filename){
+    filename %T>%
+      print() %>% 
+      read_lines(skip = 1, progress = T, locale = locale("br")) %>%
+      as_tibble() %>% 
+      separate(value, into=c("regiao","estado","municipio", "posto",
+                             "instalacao", "produto", "data","valor_compra",
+                             "valor_venda","unidade","distribuidor"), sep="  ") %>% 
+      mutate_at(vars(valor_compra, valor_venda), str_replace, ",", ".") %>%
+      mutate_at(vars(valor_compra, valor_venda), as.numeric) %>%
+      mutate_at(vars(instalacao), as.integer) %>%
+      mutate( data = dmy(data) ) %>% 
+      return()
+  }) -> prices
+
+prices %>% 
+  bind_rows() %>% 
+  mutate(
+    regiao = as.factor(regiao), 
+    estado = as.factor(estado), 
+    municipio = as.factor(municipio), 
+    posto = as.factor(posto), 
+    produto = as.factor(produto), 
+    unidade = as.factor(unidade), 
+    distribuidor = as.factor(distribuidor)
+  ) %>% 
   saveRDS("./GasPrices/data/precos.rds")
+
+
